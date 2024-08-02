e71450fc3e1d42549ad8262be48d73192943a7f7
Guilherme Fernandes <guilherme@devcaves.pt>
2024-08-02 09:23:31 +0100

    nao sei

---
+++ storage/framework/views/9f4e8fe2136a9a9bffb21c34d4f6c27c.php
@@ -0,0 +1,39 @@
+<!-- resources/views/layouts/subtopbar.blade.php -->
+
+<div class="container-fluid subtopbar-custom d-flex align-items-center justify-content-between">
+    <nav class="header-nav mx-auto">
+        <ul class="nav d-flex align-items-center">
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <img src="<?php echo e(asset('icons/league_of_legends.svg')); ?>" alt="League of Legends Icon" class="icon">
+                    <span>League of Legends</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <img src="<?php echo e(asset('icons/valorant.svg')); ?>" alt="Valorant Icon" class="icon">
+                    <span>Valorant</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <img src="<?php echo e(asset('icons/cs2.svg')); ?>" alt="CS2 Icon" class="icon">
+                    <span>CS2</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <img src="<?php echo e(asset('icons/dota_2.svg')); ?>" alt="Dota 2 Icon" class="icon">
+                    <span>Dota 2</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <img src="<?php echo e(asset('icons/rocket_league.svg')); ?>" alt="Rocket League Icon" class="icon">
+                    <span>Rocket League</span>
+                </a>
+            </li>
+        </ul>
+    </nav><!-- End Icons Navigation -->
+</div><!-- End Subtopbar -->
+<?php /**PATH /var/www/GEsports/resources/views/layouts/subtopbar.blade.php ENDPATH**/ ?>
