4104a4d013bc65b7a4230e0ddeed3dec7e355b38
Guilherme Fernandes <guilherme@devcaves.pt>
2024-08-02 17:27:56 +0100

    Accordion pagina principal

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,47 +5,62 @@
         @if (!empty($todayMatches))
             @php
                 $currentTournament = '';
+                $tournamentIndex = 0;
             @endphp
-            <div class="league">
+            <div class="accordion" id="accordionExample">
                 @foreach($todayMatches as $match)
                     @if ($currentTournament !== $match['Name'])
                         @if ($currentTournament !== '')
-            </div> <!-- Close the previous tournament div -->
-        @endif
-        @php
-            $currentTournament = $match['Name'];
-        @endphp
-        <div class="tournament">
-            <h2>{{ $currentTournament }}</h2>
-            @endif
-            <div class="match transition-fast">
-                <div class="status">{{ $match['status'] }}</div>
-                <div class="datetime">{{ date('G:i', strtotime($match['DateTime UTC'])) }}</div>
-                <div class="teams">
-                    <div class="team1">
-                        <img src="{{ $match['Team1Image'] }}" alt="{{ $match['Team1'] }}"/>
-                        {{ $match['Team1'] }}
-                    </div>
-                    <div class="team2">
-                        <img src="{{ $match['Team2Image'] }}" alt="{{ $match['Team2'] }}"/>
-                        {{ $match['Team2'] }}
-                    </div>
-                </div>
-                <div class="result">
-                    <div class="result1">{{ $match['Team1Score'] ?? '-' }}</div>
-                    <div class="result2">{{ $match['Team2Score'] ?? '-' }}</div>
-                </div>
-                <div class="stream">
-                    @if(!empty($match['Stream']))
-                        <a href="{{ $match['Stream'] }}" target="_blank">
-                            <img src="{{ asset('img/tv_icon.png') }}" alt="Stream Link">
-                        </a>
-                    @else
-                    @endif
-                </div>
             </div>
-            @endforeach
-        </div>
+    </div>
+    @endif
+    @php
+        $currentTournament = $match['Name'];
+        $tournamentIndex++;
+    @endphp
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="heading{{ $tournamentIndex }}">
+            <button class="accordion-button" type="button"
+                    data-bs-toggle="collapse" data-bs-target="#collapse{{ $tournamentIndex }}"
+                    aria-expanded="true"
+                    aria-controls="collapse{{ $tournamentIndex }}">
+                {{ $currentTournament }}
+            </button>
+        </h2>
+        <div id="collapse{{ $tournamentIndex }}"
+             class="accordion-collapse collapse show"
+             aria-labelledby="heading{{ $tournamentIndex }}" data-bs-parent="#accordion{{ $tournamentIndex }}">
+            <div class="accordion-body">
+                @endif
+                <div class="match transition-fast">
+                    <div class="status">{{ $match['status'] }}</div>
+                    <div class="datetime">{{ date('G:i', strtotime($match['DateTime UTC'])) }}</div>
+                    <div class="teams">
+                        <div class="team1">
+                            <img src="{{ $match['Team1Image'] }}" alt="{{ $match['Team1'] }}"/>
+                            {{ $match['Team1'] }}
+                        </div>
+                        <div class="team2">
+                            <img src="{{ $match['Team2Image'] }}" alt="{{ $match['Team2'] }}"/>
+                            {{ $match['Team2'] }}
+                        </div>
+                    </div>
+                    <div class="result">
+                        <div class="result1">{{ $match['Team1Score'] ?? '-' }}</div>
+                        <div class="result2">{{ $match['Team2Score'] ?? '-' }}</div>
+                    </div>
+                    <div class="stream">
+                        @if(!empty($match['Stream']))
+                            <a href="{{ $match['Stream'] }}" target="_blank">
+                                <img src="{{ asset('img/tv_icon.png') }}" alt="Stream Link">
+                            </a>
+                        @else
+                        @endif
+                    </div>
+                </div>
+                @endforeach
+            </div> <!-- Close the last tournament's accordion body -->
+        </div> <!-- Close the last accordion item -->
         @else
             <p>No data available.</p>
         @endif
--- storage/framework/views/1494f1bf93d8dade9bce9ffaf7131a35.php
+++ storage/framework/views/1494f1bf93d8dade9bce9ffaf7131a35.php
@@ -3,49 +3,65 @@
         <?php if(!empty($todayMatches)): ?>
             <?php
                 $currentTournament = '';
+                $tournamentIndex = 0;
             ?>
-            <div class="league">
+            <div class="accordion" id="accordionExample">
                 <?php $__currentLoopData = $todayMatches; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $match): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                     <?php if($currentTournament !== $match['Name']): ?>
                         <?php if($currentTournament !== ''): ?>
-            </div> <!-- Close the previous tournament div -->
-        <?php endif; ?>
-        <?php
-            $currentTournament = $match['Name'];
-        ?>
-        <div class="tournament">
-            <h2><?php echo e($currentTournament); ?></h2>
-            <?php endif; ?>
-            <div class="match transition-fast">
-                <div class="status"><?php echo e($match['status']); ?></div>
-                <div class="datetime"><?php echo e(date('G:i', strtotime($match['DateTime UTC']))); ?></div>
-                <div class="teams">
-                    <div class="team1">
-                        <img src="<?php echo e($match['Team1Image']); ?>" alt="<?php echo e($match['Team1']); ?>"/>
-                        <?php echo e($match['Team1']); ?>
-
-                    </div>
-                    <div class="team2">
-                        <img src="<?php echo e($match['Team2Image']); ?>" alt="<?php echo e($match['Team2']); ?>"/>
-                        <?php echo e($match['Team2']); ?>
-
-                    </div>
-                </div>
-                <div class="result">
-                    <div class="result1"><?php echo e($match['Team1Score'] ?? '-'); ?></div>
-                    <div class="result2"><?php echo e($match['Team2Score'] ?? '-'); ?></div>
-                </div>
-                <div class="stream">
-                    <?php if(!empty($match['Stream'])): ?>
-                        <a href="<?php echo e($match['Stream']); ?>" target="_blank">
-                            <img src="<?php echo e(asset('img/tv_icon.png')); ?>" alt="Stream Link">
-                        </a>
-                    <?php else: ?>
-                    <?php endif; ?>
-                </div>
             </div>
-            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-        </div>
+    </div>
+    <?php endif; ?>
+    <?php
+        $currentTournament = $match['Name'];
+        $tournamentIndex++;
+    ?>
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="heading<?php echo e($tournamentIndex); ?>">
+            <button class="accordion-button" type="button"
+                    data-bs-toggle="collapse" data-bs-target="#collapse<?php echo e($tournamentIndex); ?>"
+                    aria-expanded="true"
+                    aria-controls="collapse<?php echo e($tournamentIndex); ?>">
+                <?php echo e($currentTournament); ?>
+
+            </button>
+        </h2>
+        <div id="collapse<?php echo e($tournamentIndex); ?>"
+             class="accordion-collapse collapse show"
+             aria-labelledby="heading<?php echo e($tournamentIndex); ?>" data-bs-parent="#accordion<?php echo e($tournamentIndex); ?>">
+            <div class="accordion-body">
+                <?php endif; ?>
+                <div class="match transition-fast">
+                    <div class="status"><?php echo e($match['status']); ?></div>
+                    <div class="datetime"><?php echo e(date('G:i', strtotime($match['DateTime UTC']))); ?></div>
+                    <div class="teams">
+                        <div class="team1">
+                            <img src="<?php echo e($match['Team1Image']); ?>" alt="<?php echo e($match['Team1']); ?>"/>
+                            <?php echo e($match['Team1']); ?>
+
+                        </div>
+                        <div class="team2">
+                            <img src="<?php echo e($match['Team2Image']); ?>" alt="<?php echo e($match['Team2']); ?>"/>
+                            <?php echo e($match['Team2']); ?>
+
+                        </div>
+                    </div>
+                    <div class="result">
+                        <div class="result1"><?php echo e($match['Team1Score'] ?? '-'); ?></div>
+                        <div class="result2"><?php echo e($match['Team2Score'] ?? '-'); ?></div>
+                    </div>
+                    <div class="stream">
+                        <?php if(!empty($match['Stream'])): ?>
+                            <a href="<?php echo e($match['Stream']); ?>" target="_blank">
+                                <img src="<?php echo e(asset('img/tv_icon.png')); ?>" alt="Stream Link">
+                            </a>
+                        <?php else: ?>
+                        <?php endif; ?>
+                    </div>
+                </div>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            </div> <!-- Close the last tournament's accordion body -->
+        </div> <!-- Close the last accordion item -->
         <?php else: ?>
             <p>No data available.</p>
         <?php endif; ?>
