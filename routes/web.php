2bbf70a7ecfee56d0b4e6d5f25c17e64140fd319
Guilherme Fernandes <guilherme@devcaves.pt>
2024-12-05 18:14:30 +0000

    Removed useless coding

--- routes/web.php
+++ routes/web.php
@@ -18,32 +18,20 @@ use App\Http\Controllers\TeamController;
 |
 */
 
-// Authentication Routes
-Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
-Route::post('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
 
-// Register Routes
-Route::get('/register', [App\Http\Controllers\Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [App\Http\Controllers\Auth\RegisterController::class, 'register']);
+
 
 // API Routes
 Route::get('/lolhome', [APIController::class, 'todayMatches'])->name('lolhome');
-Route::get('/test', [APIController::class, 'index']);
 Route::get('/match/{matchId}/{date?}', [APIController::class, 'showMatch'])->name('match.show');
 
 // Team Image Route
 Route::get('/store-team-images', [TeamImageController::class, 'storeTeamImage'])->name('store.team.images');
 
-// User Profile Route
-Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
 
 // Default Welcome Route
 Route::get('/', function () {
     return redirect()->route('lolhome');
 });
-
-
-Route::get('/home-test', [APIController::class, 'todayMatches'])->name('home-test');
 
 Route::get('/team/{teamName}', [TeamController::class, 'show'])->name('team.show');
